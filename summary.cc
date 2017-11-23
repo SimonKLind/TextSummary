0ab3c7ae28a4029c2edf9be3c5799f5c6186ad11
Simon Andersson <si8270an-s@student.lu.se>
2017-11-23 16:59:49 +0100

    Update summary.cc

--- summary.cc
+++ summary.cc
@@ -20,12 +20,29 @@ void count_words(const char *str, WordMap &words) {
 } 
 
 const char* getRaw(const char *filename) {
+    ifstream fin(filename);
+    if(!fin) return 0;
+    fin.seekg(0, fin.end);
+    unsigned int length = fin.tellg();
+    fin.seekg(0, fin.beg);
+    char *str = new char[length+1];
+    fin.read(str, length);
+    str[length] = 0;
+    fin.close();
+    return str;
 }
 
 void get_sentences(const char *str, WordMap &words, SentenceArray &sentences) {
 }
 
 void save_summary(SentenceArray &sentences, unsigned int num_sentences, const char *filename) {
+    sentences.sort(num_sentences);
+    ofstream fout(filename);
+    if(!fout) return;
+    for(unsigned int i=0; i<num_sentences && i<sentences.length(); ++i) {
+        printSentence(fout, sentences[i]);
+    }
+    fout.close();
 }
 
 int main() {
